5fa7f147e2e7ed46ec7603a43fb5816400f191dc
Tammy Metz <tammy@tammymetz.com>
2019-08-09 11:20:52 +0000

    Done.

--- index.js
+++ index.js
@@ -11,53 +11,69 @@ class Driver {
     this.id = ++driverId;
     store.drivers.push(this);
   }
-/*
+
   trips() {
     return store.trips.filter(
       function(trip) {
         return trip.driverId == this.id;
       }.bind(this)
-    };
+    );
   }
   passengers() {
-
+    return this.trips().map(
+      function(trip) {
+        return trip.passenger();
+      }.bind(this)
+    );
   }
-  */
 }
 
-/*
+
 class Passenger {
   constructor(name) {
     this.name = name;
     this.id = ++passengerId;
+    store.passengers.push(this);
   }
   trips() {
-
+    return store.trips.filter(
+      function(trip) {
+        return trip.passengerId == this.id;
+      }.bind(this)
+    );
   }
 
   drivers() {
-
+    return this.trips().map(
+      function(trip) {
+        return trip.driver();
+      }.bind(this)
+    );
   }
 }
 
 class Trip {
   constructor(driver, passenger) {
-    this.id = ++tripID;
+    this.id = ++tripId;
     this.driverId = driver.id;
     this.passengerId = passenger.id;
     store.trips.push(this);
   }
 
   driver() {
+    return store.drivers.find(
+      function(driver) {
+        return driver.id === this.driverId;
+      }.bind(this)
+    );
+  }
 
+  passenger() {
+    return store.passengers.find(
+      function(passenger) {
+        return passenger.id === this.passengerId;
+      }.bind(this)
+    );
   }
 
 }
-
-/*
-A driver has many trips, and has many passengers through trips.
-A passenger has many trips, and has many drivers through trips.
-A trip belongs to a driver and belongs to a passenger.
-driver() - returns the driver associated with the trip
-passenger() - returns the passenger associated with the trip
-*/
